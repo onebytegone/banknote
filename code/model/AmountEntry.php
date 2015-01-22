3e81adfb7de58486504465fb9a7bd16cfa023e62
Ethan Smith <ethan@onebytegone.com>
2015-01-22 12:09:13 -0500

    Added AmountEntry

---
+++ code/model/AmountEntry.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Stores an amount.
+ *
+ * @copyright 2015 Ethan Smith
+ */
+
+class AmountEntry extends TemporalItem {
+    public $amount = 0;
+}
--- code/require.php
+++ code/require.php
@@ -4,5 +4,6 @@ require 'model/temporal/TimePeriod.php';
 require 'model/temporal/TemporalItem.php';
 require 'model/temporal/TemporalItemStore.php';
 require 'model/IncomeEntry.php';
+require 'model/AmountEntry.php';
 require 'factory/IncomeEntryFactory.php';
 require 'calculate/IncomeCalculate.php';
