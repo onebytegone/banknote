2d300fc681b839428629d7d512b4d153694806c6
Ethan Smith <ethan@onebytegone.com>
2015-02-07 15:19:15 -0800

    Added test for TimePeriod compareTo

--- tests/model/temporal/TimePeriodTest.php
+++ tests/model/temporal/TimePeriodTest.php
@@ -5,6 +5,38 @@
  */
 class TimePeriodTest extends PHPUnit_Framework_TestCase {
 
+
+   public function testCompareTo() {
+      $janPeriod = new TimePeriod();
+      $janPeriod->startDate = "1/1";
+      $janPeriod->endDate = "1/31";
+
+      $janDupPeriod = new TimePeriod();
+      $janDupPeriod->startDate = "1/1";
+      $janDupPeriod->endDate = "1/31";
+
+      $febPeriod = new TimePeriod();
+      $febPeriod->startDate = "2/1";
+      $febPeriod->endDate = "2/29";
+
+      $marchPeriod = new TimePeriod();
+      $marchPeriod->startDate = "3/1";
+      $marchPeriod->endDate = "3/31";
+
+      $twoMonths = new TimePeriod();
+      $twoMonths->startDate = "2/1";
+      $twoMonths->endDate = "4/1";
+
+
+      $this->assertEquals(0, $janPeriod->compareTo($janDupPeriod));
+      $this->assertEquals(1, $febPeriod->compareTo($janPeriod));
+      $this->assertEquals(-1, $janPeriod->compareTo($febPeriod));
+      $this->assertEquals(-1, $janPeriod->compareTo($marchPeriod));
+      $this->assertEquals(-1, $janPeriod->compareTo($twoMonths));
+      $this->assertEquals(0, $febPeriod->compareTo($twoMonths));
+      $this->assertEquals(1, $marchPeriod->compareTo($twoMonths));
+  }
+
    public function testAreEquivalent() {
       $janPeriod = new TimePeriod();
       $janPeriod->id = "jan";
