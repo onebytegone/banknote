3be3510cadba29f72f90beaf891fcc136fd00cf4
Ethan Smith <ethan@onebytegone.com>
2015-02-12 11:14:18 -0800

    Updated lifecycle test for TemporalItemStore

--- tests/model/temporal/TemporalItemStoreTest.php
+++ tests/model/temporal/TemporalItemStoreTest.php
@@ -6,8 +6,11 @@
 class TemporalItemStoreTest extends PHPUnit_Framework_TestCase {
 
    public function testLifecycle() {
+      $timePeriods = TimePeriod::all_time_periods();
       $itemA = new TemporalItem();
+      $itemA->timePeriod = $timePeriods[1];
       $itemB = new TemporalItem();
+      $itemB->timePeriod = $timePeriods[2];
       $items = array($itemA, $itemB);
          
       $store = new TemporalItemStore(array($itemA));
