a551789be727f7045d32cf43908e964a2297784a
Ethan Smith <ethan@onebytegone.com>
2015-02-15 14:36:11 -0800

    Added find by id test

--- tests/model/temporal/TemporalItemStoreTest.php
+++ tests/model/temporal/TemporalItemStoreTest.php
@@ -18,5 +18,21 @@ class TemporalItemStoreTest extends PHPUnit_Framework_TestCase {
       
       $this->assertEquals($items, $store->allItems());
    }
+   
+   public function testFindByID() {
+      $timePeriods = TimePeriod::all_time_periods();
+      $itemA = new TemporalItem();
+      $itemA->timePeriod = $timePeriods[1];
+      $itemA->id = "first";
+      $itemB = new TemporalItem();
+      $itemB->timePeriod = $timePeriods[2];
+      $itemB->id = "second";
+         
+      $store = new TemporalItemStore(array($itemA, $itemB));
+      
+      $this->assertEquals($itemA, $store->itemWithID("first"));
+      $this->assertNull($store->itemWithID("unknown"));
+      
+   }
 }
 
