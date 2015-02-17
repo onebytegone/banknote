0cdc2ac7b7e0a8875567d9bb55919d036464f0ce
Ethan Smith <ethan@onebytegone.com>
2015-02-17 18:51:05 -0500

    Updated TemporalItemStore tests

--- tests/model/temporal/TemporalItemStoreTest.php
+++ tests/model/temporal/TemporalItemStoreTest.php
@@ -7,32 +7,32 @@ class TemporalItemStoreTest extends PHPUnit_Framework_TestCase {
 
    public function testLifecycle() {
       $timePeriods = TimePeriod::all_time_periods();
-      $itemA = new TemporalItem();
-      $itemA->timePeriod = $timePeriods[1];
-      $itemB = new TemporalItem();
-      $itemB->timePeriod = $timePeriods[2];
+      $itemA = $this->makeTemporalItem("first", 1);
+      $itemB = $this->makeTemporalItem("second", 2);
       $items = array($itemA, $itemB);
-         
+
       $store = new TemporalItemStore(array($itemA));
       $store->storeItem($itemB);
-      
+
       $this->assertEquals($items, $store->allItems());
    }
-   
+
    public function testFindByID() {
       $timePeriods = TimePeriod::all_time_periods();
-      $itemA = new TemporalItem();
-      $itemA->timePeriod = $timePeriods[1];
-      $itemA->id = "first";
-      $itemB = new TemporalItem();
-      $itemB->timePeriod = $timePeriods[2];
-      $itemB->id = "second";
-         
+      $itemA = $this->makeTemporalItem("first", 1);
+      $itemB = $this->makeTemporalItem("second", 2);
+
       $store = new TemporalItemStore(array($itemA, $itemB));
-      
+
       $this->assertEquals($itemA, $store->itemWithID("first"));
       $this->assertNull($store->itemWithID("unknown"));
-      
+   }
+
+   private function makeTemporalItem($id, $timePeriodIndex) {
+      $item = new TemporalItem();
+      $item->id = $id;
+      $item->timePeriod = TimePeriod::all_time_periods()[$timePeriodIndex];
+      return $item;
    }
 }
 
