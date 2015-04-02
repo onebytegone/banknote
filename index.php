f46040a6195ed285bda25e1f6db470f9ff8d31ef
Ethan Smith <ethan@onebytegone.com>
2015-04-01 21:41:57 -0400

    Updated index to use demo data source

--- index.php
+++ index.php
@@ -3,21 +3,13 @@
 require 'environment.php';
 require 'code/require.php';
 
-// NOTE: temp data. should be paresed from user data source
-$incomeEntries = new TemporalItemStore();
-$incomeToFund = new TemporalItemStore();
-$expenseEntries = new TemporalItemStore();
-$fundDraw = new TemporalItemStore();
 
+function getJSON($filename) {
+   return json_decode(file_get_contents($filename), true);
+}
 
-
-// Package setup
-$initialPackage = array(
-   'sto_usr_income_entries' => $incomeEntries,
-   'sto_usr_income_to_fund' => $incomeToFund,
-   'sto_usr_expense_entries' => $expenseEntries,
-   'sto_usr_fund_draw' => $fundDraw,
-);
+$converter = new SourceDataConverter(getJSON('config/source-conversion.json'));
+$initialPackage = $converter->convertInput(getJSON('demo/demo-data.json'));
 
 // Setup data steps
 $stepConfig = json_decode(file_get_contents('config/data-steps.json'), true);
